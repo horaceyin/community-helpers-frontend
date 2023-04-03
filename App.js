58ca16ec1898189bfcf685f08359f2a4dfb43101
Anthony Lau <44336098+anthonylau323@users.noreply.github.com>
2023-04-04 03:15:05 +0800

    setup for push notification

--- App.js
+++ App.js
@@ -10,13 +10,22 @@ import {
 import { setContext } from "@apollo/client/link/context";
 import MainNavigator from "./src/navigations/MainNavigator";
 import * as SecureStore from "expo-secure-store";
-import { AppProvider } from "./AppContext";
-import { BASE_URL, tokenName } from "./config";
-
-import { store } from "./src/store";
-import { Provider } from "react-redux";
-import { AppGuard } from "./src/features/AppGuard";
+import { AppProvider } from './AppContext';
+import { BASE_URL, tokenName } from './config';
+import { store } from './src/store';
+import { Provider } from 'react-redux';
+import { AppGuard } from './src/features/AppGuard';
+import { useEffect, useState, useRef } from 'react';
 import { Provider as PaperProvider, DefaultTheme } from "react-native-paper";
+import * as Device from 'expo-device'
+import * as Notifications from "expo-notifications"
+Notifications.setNotificationHandler({
+  handleNotification: async () => ({
+    shouldShowAlert: true,
+    shouldPlaySound: false,
+    shouldSetBadge: false,
+  }),
+});
 
 const httpLink = createHttpLink({
   uri: BASE_URL,
@@ -41,6 +50,37 @@ const client = new ApolloClient({
   link: authLink.concat(httpLink),
   cache: new InMemoryCache(),
 });
+
+async function registerForPushNotificationsAsync() {
+  let token;
+  if (Device.isDevice) {
+    const { status: existingStatus } = await Notifications.getPermissionsAsync();
+    let finalStatus = existingStatus;
+    if (existingStatus !== 'granted') {
+      const { status } = await Notifications.requestPermissionsAsync();
+      finalStatus = status;
+    }
+    if (finalStatus !== 'granted') {
+      alert('Failed to get push token for push notification!');
+      return;
+    }
+    token = (await Notifications.getExpoPushTokenAsync()).data;
+    console.log("This is the token: " + token);
+  } else {
+    alert('Must use physical device for Push Notifications');
+  }
+
+  if (Platform.OS === 'android') {
+    Notifications.setNotificationChannelAsync('default', {
+      name: 'default',
+      importance: Notifications.AndroidImportance.MAX,
+      vibrationPattern: [0, 250, 250, 250],
+      lightColor: '#FF231F7C',
+    });
+  }
+
+  return token;
+}
 
 // entry point
 export default function App() {
@@ -69,6 +109,31 @@ export default function App() {
     },
     dark: true,
   };
+
+  const [expoPushToken, setExpoPushToken] = useState('');
+  const [notification, setNotification] = useState(false);
+  const notificationListener = useRef();
+  const responseListener = useRef();
+
+
+  useEffect(() => {
+    registerForPushNotificationsAsync().then(token => setExpoPushToken(token));
+
+    notificationListener.current = Notifications.addNotificationReceivedListener(notification => {
+      setNotification(notification);
+    });
+
+    responseListener.current = Notifications.addNotificationResponseReceivedListener(response => {
+      console.log(response);
+    });
+
+    return () => {
+      Notifications.removeNotificationSubscription(notificationListener.current);
+      Notifications.removeNotificationSubscription(responseListener.current);
+    };
+  }, []);
+
+  
   const [appLoaded] = useFonts({
     InterBold: require("./assets/fonts/Inter-Bold.ttf"),
     InterSemiBold: require("./assets/fonts/Inter-SemiBold.ttf"),
