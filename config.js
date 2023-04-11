12397a0fa129dd5a1453d30842efb411ce92b8fe
Anthony Lau <anthonylau323@gmail.com>
2023-04-11 22:00:54 +0800

    change back BASE_URL to online database

--- config.js
+++ config.js
@@ -3,8 +3,8 @@ import * as SecureStore from "expo-secure-store";
 import { setContext } from '@apollo/client/link/context';
 import { useContext } from 'react';
 
-//export const BASE_URL = 'https://communityhelper-student.azurewebsites.net/graphql';
-export const BASE_URL = 'http://192.168.1.7:3000/graphql';
+export const BASE_URL = 'https://communityhelper-student.azurewebsites.net/graphql';
+//export const BASE_URL = 'http://192.168.1.7:3000/graphql';
 // export const BASE_URL = 'http://10.7.115.18:3000/graphql';
 //export const BASE_URL = 'http://192.168.8.5:3000/graphql';
 export const tokenName = 'token';
