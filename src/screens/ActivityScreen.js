5d2274ed7a14d7858f7482c6fc66a8252785aa11
WoodyWong <37654138+WoodyWSL@users.noreply.github.com>
2023-04-16 03:56:25 +0800

    Finish a basic activity screen layout

--- src/screens/ActivityScreen.js
+++ src/screens/ActivityScreen.js
@@ -1,13 +1,77 @@
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, View, FlatList } from "react-native";
 import React from "react";
-import { SHADOWS, COLORS, SIZES, assets } from "../../constants";
-import { RectButton } from "../components";
-import {} from "react-native-paper";
+import { SHADOWS, COLORS, SIZES, assets, FONTS } from "../../constants";
+import { RectButton, JobCardDetail, JobsDetails, CandidateHelper } from "../components";
+import { RadioButton } from "react-native-paper";
+
+var description = "dfsdfsddfjl;gh klsdfjghjkldf shgjkdfsh gjkfsdh kjghdfsj ghklhdf skjhgkdfhks jfdsfsdf"
+const helpRequest = {
+  "image": assets.food,
+  "helpRequestDate": "2020-12-12",
+  "helpRequestTime": "12:23:02",
+  "title": "Help Request Title",
+  "helpSeeker": {"displayName": "helpSeeker displayName"},
+  "location": "Help Request Location",
+  "price": 20,
+  "description": description,
+  "takenHelpRequest": [
+    {"userId": "4", "user": {"displayName": "displayName 4"}},
+    {"userId": "3", "user": {"displayName": "displayName 3"}}
+  ]
+}
 
 const ActivityScreen = () => {
+  const [checked, setChecked] = React.useState(helpRequest.takenHelpRequest[0].userId);
+  console.log(checked);
+
   return (
     <View style={styles.container}>
-      <Text>ActivityScreen</Text>
+      <FlatList
+        data={helpRequest.takenHelpRequest}
+        renderItem={({ item }) => <CandidateHelper helper={item} checked={checked} setChecked={setChecked}/>}
+        keyExtractor={(item) => item.userId}
+        showsVerticalScrollIndicator={false}
+        // contentContainerStyle={{ paddingBottom: SIZES.base }}
+        ListHeaderComponent={() => (
+          <>
+            <JobCardDetail helpRequest={helpRequest} />
+            <View style={{ padding: SIZES.font }}>
+              <Text
+                style={{
+                  fontFamily: FONTS.bold,
+                  fontSize: SIZES.large,
+                  color: COLORS.primary,
+                }}
+              >
+                Matched Candidate Helper 
+              </Text>
+            </View>
+          </>
+        )}
+        ListFooterComponent={()=> (
+          <>
+            <View
+              style={{
+                width: "100%",
+                padding: SIZES.base,
+                justifyContent: "center",
+                alignItems: "center",
+                zIndex: 1,
+              }}
+            >
+              <RectButton
+                buttonText={"Accept Helper"}
+                minWidth={"100%"}
+                fontSize={SIZES.font}
+                handlePress={() =>
+                  {}
+                }
+              />
+            </View>
+
+          </>
+        )}
+      />
     </View>
   );
 };
@@ -17,7 +81,7 @@ export default ActivityScreen;
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
+    // justifyContent: "center",
+    // alignItems: "center",
   },
 });
